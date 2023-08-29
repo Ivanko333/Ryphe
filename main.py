726b221759e533f1d0fdeadf07454d436a0f24ef
Your a <a@example.com>
2023-08-29 20:26:24 +0300

    edited

---
+++ main.py
@@ -0,0 +1,14 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget
+
+
+class MainWin(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.set_appear()
+
+    def set_appear(self):
+        pass
+
+
+
