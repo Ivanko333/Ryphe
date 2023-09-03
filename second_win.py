4aa05c740228eb11249dd3741aa75d37522d83fc
Your a <a@example.com>
2023-09-03 13:24:04 +0300

    edited1

--- second_win.py
+++ second_win.py
@@ -1,11 +1,15 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QHBoxLayout, QLineEdit
 from instr import *
+from third_win import ThirdWin
 
 
 class SecondWin(QWidget):
     def __init__(self):
         super().__init__()
+        self.button4 = None
+        self.text_timer = None
+        self.third_win = None
         self.initUI()
         self.connects()
         self.set_appear()
@@ -21,10 +25,11 @@ class SecondWin(QWidget):
         test1 = QLabel(txt_test1)
         test2 = QLabel(txt_test2)
         test3 = QLabel(txt_test3)
+        self.text_timer = QLabel('00:00:00')
         button1 = QPushButton(txt_starttest1)
         button2 = QPushButton(txt_starttest2)
         button3 = QPushButton(txt_starttest3)
-        button4 = QPushButton(txt_sendresults)
+        self.button4 = QPushButton(txt_sendresults)
         h_line = QHBoxLayout()
         v_line1 = QVBoxLayout()
         v_line2 = QVBoxLayout()
@@ -47,10 +52,15 @@ class SecondWin(QWidget):
         v_line1.addWidget(button3, alignment=Qt.AlignLeft)
         v_line1.addWidget(input4, alignment=Qt.AlignLeft)
         v_line1.addWidget(input5, alignment=Qt.AlignLeft)
-        v_line.addWidget(button4, alignment=Qt.AlignCenter)
+        v_line1.addWidget(self.button4, alignment=Qt.AlignCenter)
+        v_line2.addWidget(self.text_timer, alignment=Qt.AlignCenter)
         self.setLayout(v_line1)
         self.setLayout(v_line2)
         self.setLayout(h_line)
 
+    def next(self):
+        self.third_win = ThirdWin()
+        self.hide()
+
     def connects(self):
-        pass
+        self.button4.clicked.connect(self.next)
---
+++ third_win.py
@@ -0,0 +1,9 @@
+from PyQt5.QtCore import Qt, QTime, QTimer
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QHBoxLayout
+from instr import *
+
+
+class ThirdWin(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.show()
